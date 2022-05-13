09cdb792a0155dac584ea88b1cc95109027f34d1
safin777 <sayedislamsafin7770555@gmail.com>
2022-05-14 05:00:15 +0600

    socials icon added with image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/img/me.png";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <header>
+            <div className="container header__container">
+                <h2>Hello I'm </h2>
+                <h1>Safin Sayed</h1>
+                <h3 className="text-light">I'm a Full Stack Web Developer</h3>
+                <CTA/>
+                <HeaderSocials/>
+            </div>
 
-export default Header
+            <div className="me">
+              <img src={ME} alt="me"/> 
+            </div>
+
+            <a href="#contact" className="scroll_down">Scroll Down</a>
+        </header>
+    );
+};
+
+export default Header;
