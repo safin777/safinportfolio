f28b98e9cfc38fe84e3401c97aaade431dcb35cc
safin777 <sayedislamsafin7770555@gmail.com>
2022-05-14 02:33:44 +0600

    Project Created

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Service from './components/service/Service'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+const App = () => {
+  return (
+   <>
+
+    <Header />
+    <Nav/>
+    <About />
+    <Experience />
+    <Service/>
+    <Portfolio />
+    <Testimonials />
+    <Contact />
+    <Footer />
+
+   </>
+  )
+}
+
+export default App
