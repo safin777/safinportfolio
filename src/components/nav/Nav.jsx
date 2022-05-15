839a9ef020531cf065a99d888a09f066e82cac46
safin777 <sayedislamsafin7770555@gmail.com>
2022-05-16 01:57:19 +0600

    floating nav design is done.

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,24 +5,26 @@ import { AiOutlineUser} from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDots } from "react-icons/bi";
+import { useState } from 'react';
 
 
 const Nav = () => {
+    const[activeNav, setActiveNav] = useState('#');
     return (
         <nav className="nav">
-            <a href="#">
+            <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
                 <AiOutlineHome />
             </a>
-            <a href="#about">
+            <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>
                 <AiOutlineUser />
             </a>
-            <a href="#experience">
+            <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>
                 <BiBook />
             </a>
-            <a href="#services">
+            <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>
                 <RiServiceLine />
             </a>
-            <a href="#contact">
+            <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
                 <BiMessageSquareDots />
             </a>
         </nav>
