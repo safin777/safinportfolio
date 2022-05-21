a473ed8dcfa674ebf5b860664f4148d0c5420381
safin777 <sayedislamsafin7770555@gmail.com>
2022-05-21 22:28:23 +0600

    experience componenet design 60% done

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,159 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+
+import { SiHtml5 } from "react-icons/si";
 
 const Experience = () => {
-  return (
-    <section id='experience'>
-      <h1>Experience</h1>
-    </section>
-  )
-}
+    return (
+        <section id="experience">
+            <h5>What Skills I Have</h5>
+            <h1>My Experiences</h1>
 
-export default Experience
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Frontend Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                {/* End of frontEnd */}
+
+                <div className="experience__backend">
+                    <h3>Backend Development</h3>
+                    <div className="experience__content">
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                        <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <SiHtml5 className="experience__icon" />
+                            <div>
+                                <h5>HTML</h5>
+                                <small className="text-light">
+                                    Experienced
+                                </small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
