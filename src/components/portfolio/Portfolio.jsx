a99aadb8700ef8bf0b675dd838560a7590073144
safin777 <sayedislamsafin7770555@gmail.com>
2022-05-24 00:12:11 +0600

    portfolio component 20% done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './portfolio.css'
+import Metro from "../../assets/img/metrocery.png";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Works....</h5>
+      <h1>Portfolio</h1>
+
+      <div className='container portfolio-container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-img'>
+            <img src={Metro} alt="metro"/>
+          </div>
+          <h3>This is portfolio item title</h3>
+          <a href='https://github.com' className='btn'>Github</a>
+          <a href='https://github.com' className='btn btn-primary'>Live</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
